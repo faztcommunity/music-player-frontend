226624922783f1cc65dc9d40cbb0c29366fbfacb
zd4y <ariasorozco03@gmail.com>
2020-06-23 14:48:32 -0500

    fix merge

--- src/containers/MusicApp/MusicApp.jsx
+++ src/containers/MusicApp/MusicApp.jsx
@@ -7,11 +7,7 @@ import UserNav from '@components/UserNav/UserNav';
 import { Switch } from 'react-router-dom';
 import useUser from '@hooks/useUser';
 
-<<<<<<< HEAD
 import { MusicPlayerProvider } from '@context/MusicPlayer';
-=======
-import { MusicPlayerProvider } from '../../context/MusicPlayer';
->>>>>>> f9360064a7dbdd4f14d67ed7ce384bf7070c6c31
 
 export default function MusicApp({ children }) {
   const { isLoggedIn } = useUser();
