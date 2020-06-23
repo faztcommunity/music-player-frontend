942c1bb9fb334b282fafffe9e9f222993cd29aa2
Fernando Lopez <lopezfer@globalhitss.com>
2020-06-23 14:00:42 -0300

    clear unnecessary imports

--- src/containers/MusicApp/MusicApp.jsx
+++ src/containers/MusicApp/MusicApp.jsx
@@ -2,10 +2,6 @@ import React, { useState } from 'react';
 import MusicPlayer from '@containers/MusicPlayer';
 import Main from '@containers/Main';
 import Sidebar from '@containers/Sidebar';
-import Search from '@components/Search';
-import Grid from '@material-ui/core/Grid';
-import { Menu, MenuItem } from '@material-ui/core';
-import CardTalent from '@components/Cards/CardTalent';
 import UserNav from '@components/UserNav/UserNav';
 
 import { Switch } from 'react-router-dom';
